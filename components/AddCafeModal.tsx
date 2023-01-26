5575aae95d7e17105241abf7df24cbcd717e7437
whitneywind <78887901+whitneywind@users.noreply.github.com>
2023-01-26 13:37:50 -0800

    dependency edit

--- components/AddCafeModal.tsx
+++ components/AddCafeModal.tsx
@@ -51,8 +51,8 @@ const AddCafe: NextPage = () => {
     }
 
     const handleSubmit = (e: SyntheticEvent) => {
+        e.preventDefault();
         console.log(newCafe)
-        
     }
 
     return (
@@ -63,9 +63,9 @@ const AddCafe: NextPage = () => {
                 <form onSubmit={handleSubmit} action="/api/registerCafe" method="post">
                     <header className="bg-[#ba997b] p-3 pt-5 text-2xl text-center text-white">Add a Cafe</header>
                     <main className="w-100 flex flex-col px-6 mb-7 md:my-8 md:space-y-4 mt-8 overflow-hidden">
-                        <input onChange={handleChange} required type="text" name="name" placeholder="name" className='mx-auto px-2 py-3 w-full text-lg' />
-                        <input onChange={handleChange} required type="text" name="address" placeholder="address" className='mx-auto px-2 py-3 w-full text-lg' />
-                        <input onChange={handleChange} required type="text" name="neighborhood" placeholder="neighborhood" className='mx-auto px-2 py-3 w-full text-lg' />
+                        <input onChange={handleChange} required type="text" name="name" placeholder="name" className='mx-auto px-2 pt-2 pb-1 w-full text-lg' />
+                        <input onChange={handleChange} required type="text" name="address" placeholder="address" className='mx-auto px-2 pt-2 pb-1 w-full text-lg' />
+                        <input onChange={handleChange} required type="text" name="neighborhood" placeholder="neighborhood" className='mx-auto px-2 pt-2 pb-1 w-full text-lg' />
                         <button className='bg-[#ba997b] w-fit px-4 rounded-md text-xl pt-3 pb-2 text-white self-center' type="submit">submit</button>
                     </main>
                 </form>
