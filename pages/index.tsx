2bd406e1036753f84cbd552109fb0daced9544c5
whitneywind <78887901+whitneywind@users.noreply.github.com>
2023-05-01 09:05:12 -0700

    bg swap

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
   }
 
   return (
-    <div className="bg-[#e4bb97]/70 h-screen overflow-hidden font-special">
+    <div className="bg-main-bg bg-center bg-no-repeat bg-cover h-screen overflow-hidden font-special">
       <nav className='flex justify-between px-5 mt-2 mx-2 rounded-md items-center bg-[#91745c]/90'>
         <Link href="/user/favorites" className=''>
           <BookmarkSquareIcon className='h-9 text-stone-200 hover:cursor-pointer hover:text-stone-400' />
@@ -24,7 +24,7 @@ const Home: NextPage = () => {
         </Link>
       </nav>
       <main className='flex items-center'>
-        <div className='bg-main-bg bg-center bg-no-repeat bg-cover overflow-hidden h-100 mt-32 w-4/5 md:w-2/3 lg:w-1/2 flex flex-col text-center mx-auto text-white py-24 pb-40 rounded'>
+        <div className='h-100 mt-32 w-4/5 md:w-2/3 lg:w-1/2 flex flex-col text-center mx-auto text-white py-24 pb-40 rounded'>
           <header className=' pt-10 pb-5'>
             <h1 className='text-3xl font-bold drop-shadow-md'>{!area ? 'select a neighborhood' : 'neighborhood:'}</h1>
           </header>
